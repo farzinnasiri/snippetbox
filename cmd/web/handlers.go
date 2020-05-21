aaef50c8a3e194bc51eeafa69917129462f03992
farzinnasiri <farzinnasiri890@gmail.com>
2020-05-21 15:30:28 +0430

    logout user

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -138,5 +138,7 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	app.session.Remove(r, "userID")
+	app.session.Put(r, "flash", "You've been logged out successfully!")
+	http.Redirect(w, r, "/", 303)
 }
