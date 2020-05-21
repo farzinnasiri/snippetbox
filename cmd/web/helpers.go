c7260d6badfd07885ecc77f31f12de66634facb6
farzinnasiri <farzinnasiri890@gmail.com>
2020-05-21 15:53:24 +0430

    user authorization added

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -43,6 +43,12 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 	if td == nil {
 		td = &templateData{}
 	}
+	td.AuthenticatedUser = app.authenticatedUser(r)
 	td.CurrentYear = time.Now().Year()
+	td.Flash = app.session.PopString(r, "flash")
 	return td
+}
+
+func (app *application) authenticatedUser(r *http.Request) int {
+	return app.session.GetInt(r, "userID")
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -9,6 +9,7 @@ import (
 )
 
 type templateData struct {
+	AuthenticatedUser int
 	CurrentYear int
 	Flash       string
 	Form        *forms.Form
